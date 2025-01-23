783e19883a561b3c5cdbb16645857e589a0b2a6a
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2025-01-23 09:20:36 -0300

    [ REFACT ] Adjusting structure of folders.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout() {
   return (
     <View className="flex-1 justify-center items-center bg-neutral-100">
       <Text className="text-red-500">Hello World!</Text>
-      <Text>Hello World 02!</Text>
+      <Text>Hello World 03!</Text>
     </View>
   );
 }
