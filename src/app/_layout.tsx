149e756c04d7c0c8625b2e7fcccfe0de9fc7a842
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2025-01-23 10:31:11 -0300

    [ REFACT ] Adjusting layout.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,30 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
 import "../styles/global.css";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
+  function handleParticipantAdd() {
+    console.log("Here!");
+  }
+
   return (
-    <View className="flex-1 justify-center items-center bg-neutral-100">
-      <Text className="text-red-500">Hello World!</Text>
-      <Text>Hello World 03!</Text>
-    </View>
+    <SafeAreaView className="mt-[10vh] px-[5vw] gap-[.85vh]">
+      <Text className="text-white font-bold text-[5vw]">Nome do evento</Text>
+      <Text className="text-gray-400 text-[3vw]">
+        Sexta, 4 de Novembro de 2022.
+      </Text>
+
+      <View className="flex-row gap-x-[3vw]">
+        <TextInput className="flex-1 h-[5vh] rounded-[1.35vw] bg-gray-700 text-white px-[4vw] text-[3.5vw]" />
+
+        <TouchableOpacity
+          className="bg-green-500 w-[10vw] h-[10vw] rounded-[1.35vw] justify-center items-center"
+          onPress={handleParticipantAdd}
+        >
+          <Text className="text-white text-[6vw]">+</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
