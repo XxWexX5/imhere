2ca5c58afd650bf1120a9570904d0f3b7b8f9b90
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2025-01-23 11:10:32 -0300

    [ FEATURE ] Creating component Participant.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,9 @@
 import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
-import "../styles/global.css";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import "../styles/global.css";
+import { Participant } from "../components/Participant";
 
 export default function RootLayout() {
   function handleParticipantAdd() {
@@ -9,21 +11,34 @@ export default function RootLayout() {
   }
 
   return (
-    <SafeAreaView className="mt-[10vh] px-[5vw] gap-[.85vh]">
-      <Text className="text-white font-bold text-[5vw]">Nome do evento</Text>
-      <Text className="text-gray-400 text-[3vw]">
-        Sexta, 4 de Novembro de 2022.
-      </Text>
+    <SafeAreaView className="mt-[2vh] px-[5vw]">
+      <View className="w-full gap-[.65vh]">
+        <Text className="text-white font-bold text-[5vw]">Nome do evento</Text>
+        <Text className="text-gray-400 text-[3vw]">
+          Sexta, 4 de Novembro de 2022.
+        </Text>
+      </View>
 
-      <View className="flex-row gap-x-[3vw]">
-        <TextInput className="flex-1 h-[5vh] rounded-[1.35vw] bg-gray-700 text-white px-[4vw] text-[3.5vw]" />
+      <View className="gap-y-[5vh]">
+        <View className="flex-row gap-x-[3vw] mt-[2vh]">
+          <TextInput
+            className="flex-1 h-[5vh] rounded-[1.35vw] bg-gray-700 text-white px-[4vw] text-[3.5vw]"
+            placeholder="Nome do participante"
+          />
 
-        <TouchableOpacity
-          className="bg-green-500 w-[10vw] h-[10vw] rounded-[1.35vw] justify-center items-center"
-          onPress={handleParticipantAdd}
-        >
-          <Text className="text-white text-[6vw]">+</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            className="bg-green-500 w-[10vw] h-[10vw] rounded-[1.35vw] justify-center items-center"
+            onPress={handleParticipantAdd}
+          >
+            <Text className="text-white text-[6vw]">+</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View className="gap-y-[1.5vh]">
+          <Participant />
+
+          <Participant />
+        </View>
       </View>
     </SafeAreaView>
   );
---
+++ src/components/Participant/index.tsx
@@ -0,0 +1,15 @@
+import { Text, TouchableOpacity, View } from "react-native";
+
+export function Participant() {
+  return (
+    <View className="flex-row gap-x-[3vw]">
+      <View className="flex-1 w-full bg-gray-700 h-[5vh] rounded-[1.35vw] px-[.5vw] text-[3.5vw] items-center flex-row">
+        <Text className="text-white px-[4vw]">Wevison Ramalho</Text>
+      </View>
+
+      <TouchableOpacity className="bg-red-500 w-[10vw] h-[10vw] rounded-[1.35vw] justify-center items-center">
+        <Text className="text-white text-[6vw]">-</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
